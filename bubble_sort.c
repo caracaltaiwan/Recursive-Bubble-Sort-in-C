79f4d52f2e6e8b4fe81545756ad026114693de02
TomTsai [蔡孟廷] <TomTsai@tw.megatrends.com>
2020-05-05 19:28:49 +0800

    Update bubble_sort.c
    Add comments.

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 void bubble_sort(int array[], int number);
-int array[10] = {10,9,6,7,8,5,4,3,2,1};
-int compareMax = sizeof(array)/sizeof(int)-1;
-int maxNumber = sizeof(array)/sizeof(int);
+
+int array[10] = {10,9,6,7,8,5,4,3,2,1};//Data input.
+int compareMax = sizeof(array)/sizeof(int)-1;//How many compares on a short cycle.
+int maxNumber = sizeof(array)/sizeof(int);//Element number present.
 int i = 0;
 
 int main(int argc, char* argv[]) {
 	bubble_sort(array, 0);
 	printf("\n");
-	for(i=0;i<maxNumber;i++){
+	for(i=0;i<maxNumber;i++)
+	{
 		printf("\n%d",array[i]);
 	}
-	//getchar();// Pause
-
+	//getchar();//Pause
     return 0; 
 }
 
@@ -21,9 +22,9 @@ void bubble_sort(int array[], int number)
 {
 	int temp;
 	
-	printf("\n%d %d",array[number],array[number+1]);
+	//printf("\n%d %d",array[number],array[number+1]);//Verbose Message
 	if(number!=compareMax){
-		printf("\narray[number]:%d,array[number+1]:%d,number:%d",array[number],array[number+1],number);
+		//printf("\narray[number]:%d,array[number+1]:%d,number:%d",array[number],array[number+1],number);//Verbose Message
 		if(array[number]>array[number+1]||array[number]==array[number+1])
 		{
 			temp = array[number+1];
@@ -37,9 +38,14 @@ void bubble_sort(int array[], int number)
 			return bubble_sort(array, number+1);
 		}
 	}else{
-		if(compareMax>0){
+		if(compareMax>0)
+		{
 			compareMax-=1;
 			return bubble_sort(array, 0);
+		}
+		else if
+		{
+			return 0;
 		}
 	}
 }
